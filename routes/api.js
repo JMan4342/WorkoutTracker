ee578451da3e9a675ea6f5cdd9fa7f3e6d385cf5
Joseph Manning <jgmanning3@yahoo.com>
2021-07-25 17:07:09 -0700

    Create api route file

---
+++ routes/api.js
@@ -0,0 +1,76 @@
+app.post("POST", ({ body }, res) => {
+    Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  // app.get("/read", (req, res) => {
+  //   db.day.find({ read: true }, (error, found) => {
+  //     if (error) {
+  //       console.log(error);
+  //     } else {
+  //       res.json(found);
+  //     }
+  //   });
+  // });
+  
+  // app.get("/unread", (req, res) => {
+  //   db.day.find({ read: false }, (error, found) => {
+  //     if (error) {
+  //       console.log(error);
+  //     } else {
+  //       res.json(found);
+  //     }
+  //   });
+  // });
+  
+  // app.put("/markread/:id", ({ params }, res) => {
+  //   db.day.update(
+  //     {
+  //       _id: mongojs.ObjectId(params.id)
+  //     },
+  //     {
+  //       $set: {
+  //         read: true
+  //       }
+  //     },
+  
+  //     (error, edited) => {
+  //       if (error) {
+  //         console.log(error);
+  //         res.send(error);
+  //       } else {
+  //         console.log(edited);
+  //         res.send(edited);
+  //       }
+  //     }
+  //   );
+  // });
+  
+  // app.put("/markunread/:id", ({ params }, res) => {
+  //   db.day.update(
+  //     {
+  //       _id: mongojs.ObjectId(params.id)
+  //     },
+  //     {
+  //       $set: {
+  //         read: false
+  //       }
+  //     },
+  
+  //     (error, edited) => {
+  //       if (error) {
+  //         console.log(error);
+  //         res.send(error);
+  //       } else {
+  //         console.log(edited);
+  //         res.send(edited);
+  //       }
+  //     }
+  //   );
+  // });
+  
--- server.js
+++ server.js
@@ -13,89 +13,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workoutdb", 
+  { 
+    useNewUrlParser: true 
+  });
 
 
 // db.on("error", error => {
 //   console.log("Database Error:", error);
 // });
 
-app.post("POST", ({ body }, res) => {
-  Workout.create(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-// app.get("/read", (req, res) => {
-//   db.day.find({ read: true }, (error, found) => {
-//     if (error) {
-//       console.log(error);
-//     } else {
-//       res.json(found);
-//     }
-//   });
-// });
-
-// app.get("/unread", (req, res) => {
-//   db.day.find({ read: false }, (error, found) => {
-//     if (error) {
-//       console.log(error);
-//     } else {
-//       res.json(found);
-//     }
-//   });
-// });
-
-// app.put("/markread/:id", ({ params }, res) => {
-//   db.day.update(
-//     {
-//       _id: mongojs.ObjectId(params.id)
-//     },
-//     {
-//       $set: {
-//         read: true
-//       }
-//     },
-
-//     (error, edited) => {
-//       if (error) {
-//         console.log(error);
-//         res.send(error);
-//       } else {
-//         console.log(edited);
-//         res.send(edited);
-//       }
-//     }
-//   );
-// });
-
-// app.put("/markunread/:id", ({ params }, res) => {
-//   db.day.update(
-//     {
-//       _id: mongojs.ObjectId(params.id)
-//     },
-//     {
-//       $set: {
-//         read: false
-//       }
-//     },
-
-//     (error, edited) => {
-//       if (error) {
-//         console.log(error);
-//         res.send(error);
-//       } else {
-//         console.log(edited);
-//         res.send(edited);
-//       }
-//     }
-//   );
-// });
 
 app.listen(PORT, () => {
-  console.log("App running on port ${PORT}!");
+  console.log(`App running on port ${PORT}!`);
 });
