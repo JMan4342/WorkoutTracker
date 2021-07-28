2f17ccd8e2a5e6149f9322ec076134211b9e6617
Joseph Manning <jgmanning3@yahoo.com>
2021-07-27 21:15:57 -0700

    Build routes

--- routes/api.js
+++ routes/api.js
@@ -1,99 +1,117 @@
-const router = require('express').Router();
-const Workout = require('../models/workout');
+const router = require("express").Router();
+const Workout = require("../models/workout");
 
 router.post("/api/workouts", ({ body }, res) => {
-    Workout.create(body)
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-  
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 router.get("/api/workouts", (req, res) => {
-  Workout.aggregate(
-    [
-      {
-        $addFields:
-          {
-            totalDuration: { $sum: '$exercises.duration' },
-            // totalWeight: { $sum: { $multiply: [ "$weight", "$reps", "$sets" ] } },
-          }
-      }
-    ]
- )
-  .then(dbWorkout => {
-    res.json(dbWorkout);
-  })
-  .catch(err => {
-    res.json(err);
-  });
-})
-  // app.get("/read", (req, res) => {
-  //   db.day.find({ read: true }, (error, found) => {
-  //     if (error) {
-  //       console.log(error);
-  //     } else {
-  //       res.json(found);
-  //     }
-  //   });
-  // });
-  
-  // app.get("/unread", (req, res) => {
-  //   db.day.find({ read: false }, (error, found) => {
-  //     if (error) {
-  //       console.log(error);
-  //     } else {
-  //       res.json(found);
-  //     }
-  //   });
-  // });
-  
-  // app.put("/markread/:id", ({ params }, res) => {
-  //   db.day.update(
-  //     {
-  //       _id: mongojs.ObjectId(params.id)
-  //     },
-  //     {
-  //       $set: {
-  //         read: true
-  //       }
-  //     },
-  
-  //     (error, edited) => {
-  //       if (error) {
-  //         console.log(error);
-  //         res.send(error);
-  //       } else {
-  //         console.log(edited);
-  //         res.send(edited);
-  //       }
-  //     }
-  //   );
-  // });
-  
-  // app.put("/markunread/:id", ({ params }, res) => {
-  //   db.day.update(
-  //     {
-  //       _id: mongojs.ObjectId(params.id)
-  //     },
-  //     {
-  //       $set: {
-  //         read: false
-  //       }
-  //     },
-  
-  //     (error, edited) => {
-  //       if (error) {
-  //         console.log(error);
-  //         res.send(error);
-  //       } else {
-  //         console.log(edited);
-  //         res.send(edited);
-  //       }
-  //     }
-  //   );
-  // });
-  
-  module.exports = router;
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+        // totalWeight: { $sum: { $multiply: [ "$weight", "$reps", "$sets" ] } },
+      },
+    },
+  ])
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put("/api/workouts/" + id, (req, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// app.get("/read", (req, res) => {
+//   db.day.find({ read: true }, (error, found) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       res.json(found);
+//     }
+//   });
+// });
+
+// app.get("/unread", (req, res) => {
+//   db.day.find({ read: false }, (error, found) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       res.json(found);
+//     }
+//   });
+// });
+
+// app.put("/markread/:id", ({ params }, res) => {
+//   db.day.update(
+//     {
+//       _id: mongojs.ObjectId(params.id)
+//     },
+//     {
+//       $set: {
+//         read: true
+//       }
+//     },
+
+//     (error, edited) => {
+//       if (error) {
+//         console.log(error);
+//         res.send(error);
+//       } else {
+//         console.log(edited);
+//         res.send(edited);
+//       }
+//     }
+//   );
+// });
+
+// app.put("/markunread/:id", ({ params }, res) => {
+//   db.day.update(
+//     {
+//       _id: mongojs.ObjectId(params.id)
+//     },
+//     {
+//       $set: {
+//         read: false
+//       }
+//     },
+
+//     (error, edited) => {
+//       if (error) {
+//         console.log(error);
+//         res.send(error);
+//       } else {
+//         console.log(edited);
+//         res.send(edited);
+//       }
+//     }
+//   );
+// });
+
+module.exports = router;
